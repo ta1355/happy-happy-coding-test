0781b3260fe551c41a99c503842b1f5bde4330d8
ta1355 <eorbs255@naver.com>
2024-12-10 23:11:49 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.next() + "??!");
+    }
+}
